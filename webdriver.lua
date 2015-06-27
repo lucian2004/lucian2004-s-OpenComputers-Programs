aaa9fc4e410eb3c4278b964ea3cd56eb93addf4b
lucian2004 <lucrobloxgaming@gmail.com>
2015-06-26 22:49:10 -0700

    Update webdriver.lua

--- webdriver.lua
+++ webdriver.lua
@@ -1,9 +1,11 @@
 -- Useless program used to test computer audio
 -- Made by lucian2004
--- Pastebin File: http://pastebin.com/aw1caH3C (ID: aw1caH3C)
+-- Check back at the GitHub file frequently to see if there ane any patches or updates to the program!
 
 local computer = require "computer"
 local term = require "term"
+local PROGVER = 1.1
+local PROGBUILD = 164
 
 --SET TIME BETWEEN BEEPS (IN SECONDS)
 local waittime = SETME
@@ -13,11 +15,22 @@ error("You forgot to set waittime!")
 else
 print("waitTime: "..waittime)
 print("Value Check Complete... Starting Up!")
-os.sleep(1)
+os.sleep(2)
 term.clear()
 end
 
+print("Welcome to Webdriver! This program is used to test OpenComputers System Audio!")
+print("Current Version: " ..PROGVER)
+print("Current Build: " ..PROGBUILD)
+computer.beep(300)
+computer.beep(600)
+computer.beep(900)
+computer.beep(600)
+computer.beep(1200)
+os.sleep(5)
+
 while true do
+  print("Press Ctrl+Alt+C at any time to stop the program")
 local tone = math.random(20,2000)
 computer.beep(tone)
 print(tone)
